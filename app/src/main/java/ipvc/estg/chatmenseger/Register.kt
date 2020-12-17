a63341c547fdb68795c97e860f9ef9392b741a24
joelvaz99 <joelvaz@ipvc.pt>
2020-12-17 17:15:12 +0000

    Initial commit

---
+++ app/src/main/java/ipvc/estg/chatmenseger/Register.kt
@@ -0,0 +1,11 @@
+package ipvc.estg.chatmenseger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
