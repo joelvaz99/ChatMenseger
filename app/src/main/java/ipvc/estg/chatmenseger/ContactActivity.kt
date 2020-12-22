15a45b57b5008d2384caea08b9f12a47a2c8fef2
joelvaz99 <joelvaz@ipvc.pt>
2020-12-22 22:02:03 +0000

    Send Message and list message

--- app/src/main/java/ipvc/estg/chatmenseger/ContactActivity.kt
+++ app/src/main/java/ipvc/estg/chatmenseger/ContactActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
